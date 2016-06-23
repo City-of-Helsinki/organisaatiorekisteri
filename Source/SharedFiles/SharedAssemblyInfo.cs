8255756056bd1465c02cd17f3ee110c89d045eef
Eero Parkkonen <eero.parkkonen@affecto.com>
2016-06-23 09:46:30 +0300

    release note update & raise version number

--- Source/SharedFiles/SharedAssemblyInfo.cs
+++ Source/SharedFiles/SharedAssemblyInfo.cs
@@ -2,6 +2,6 @@
 
 [assembly: AssemblyProduct("Organisaatiorekisteri")]
 
-[assembly: AssemblyVersion("0.3.0.0")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
-[assembly: AssemblyInformationalVersion("0.3.0")]
+[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
+[assembly: AssemblyInformationalVersion("0.4.0")]
