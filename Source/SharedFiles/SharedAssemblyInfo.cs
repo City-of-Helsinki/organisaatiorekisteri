b68ea43bfa91d3c6a67f442115884bc9ef8500b1
Tommi Sjögren <tommi.sjogren@affecto.com>
2016-09-30 14:58:25 +0300

    Version number increment

--- Source/SharedFiles/SharedAssemblyInfo.cs
+++ Source/SharedFiles/SharedAssemblyInfo.cs
@@ -2,6 +2,6 @@
 
 [assembly: AssemblyProduct("Organisaatiorekisteri")]
 
-[assembly: AssemblyVersion("0.6.0.0")]
-[assembly: AssemblyFileVersion("0.6.0.0")]
-[assembly: AssemblyInformationalVersion("0.6.0")]
+[assembly: AssemblyVersion("0.7.0.0")]
+[assembly: AssemblyFileVersion("0.7.0.0")]
+[assembly: AssemblyInformationalVersion("0.7.0")]
