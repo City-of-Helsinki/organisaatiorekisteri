3ca37ca530a01df0f574d3d78e5e506b16c7cda7
Tommi Sjögren <tommi.sjogren@affecto.com>
2016-12-01 17:32:51 +0200

    Added parentId to organisation query JSON results.

--- Source/Api/Organization/HierarchicalOrganization.cs
+++ Source/Api/Organization/HierarchicalOrganization.cs
@@ -7,6 +7,7 @@ namespace OrganizationRegister.Api.Organization
     public class HierarchicalOrganization
     {
         public Guid Id { get; set; }
+        public Guid? ParentId { get; set; }
         public IEnumerable<LocalizedText> Names { get; set; }
         public string ValidFrom { get; set; }   //string, contains only date part in yyyy-MM-dd format
         public string ValidTo { get; set; }     //string, contains only date part in yyyy-MM-dd format
