fbc02c48eaca1624bbe9e0d673005e22b0cbe6df
Eero Parkkonen <eero.parkkonen@affecto.com>
2017-01-26 13:52:14 +0200

    Fix: Login redirects to error page if no permissions

--- Source/AngularApplication/App/Controllers/LoginController.ts
+++ Source/AngularApplication/App/Controllers/LoginController.ts
@@ -8,8 +8,9 @@ module OrganizationRegister
         public emailAddress: string;
         public password: string;
         public loginFailed: boolean;
-        
+
         private requestedRoute: string;
+
 
         constructor(private $scope: Affecto.Base.IViewScope, private $location: angular.ILocationService, private authenticationService: Affecto.Login.IAuthenticationService,
             private busyIndicationService: Affecto.BusyIndication.IBusyIndicationService)
@@ -20,7 +21,7 @@ module OrganizationRegister
         }
 
         public login(): void
-        {            
+        {
             this.busyIndicationService.showBusyIndicator("Kirjaudutaan sisään...");
 
             this.authenticationService
@@ -28,40 +29,41 @@ module OrganizationRegister
                 .then(this.onLogInCompleted, this.onLogInError);
         }
 
-        private checkPermission(): void
+        private hasRequiredPermission(): boolean
         {
             var user: AuthenticatedUser = this.authenticationService.getUser<AuthenticatedUser>();
-            if (!user.hasPermission(Permission.maintenanceOfUsers) && !user.hasPermission(Permission.maintenanceOfOrganizationData))
-                {
-                    
-                    this.authenticationService.logOut();
-                    this.busyIndicationService.hideBusyIndicator();
-                    this.$location
-                        .path(Affecto.ExceptionHandling.Routes.error)
-                        .search(Affecto.ExceptionHandling.UrlParameters.errorCode, ErrorCode.insufficientPermissions);
-                }
+            return (user.hasPermission(Permission.maintenanceOfUsers) ||
+                user.hasPermission(Permission.maintenanceOfOrganizationData));
         }
 
 
-        private onLogInCompleted = (): void=>
+        private onLogInCompleted = (): void =>
         {
-            this.checkPermission();
-
-            this.loginFailed = false;
-
-            if (this.requestedRoute && this.requestedRoute !== Route.login)
+            if (this.hasRequiredPermission())
             {
-                this.$location.search(UrlParameter.requestedRoute, null);
-                this.$location.path(this.requestedRoute);
+                this.loginFailed = false;
+
+                if (this.requestedRoute && this.requestedRoute !== Route.login)
+                {
+                    this.$location.search(UrlParameter.requestedRoute, null);
+                    this.$location.path(this.requestedRoute);
+                }
+                else
+                {
+                    this.$location.path(Route.frontPage);
+                }
             }
             else
             {
-                this.$location.path(Route.frontPage);
+                this.loginFailed = true;
+                this.authenticationService.logOut();
+                this.$location
+                    .path(Affecto.ExceptionHandling.Routes.error)
+                    .search(Affecto.ExceptionHandling.UrlParameters.errorCode, ErrorCode.insufficientPermissions);
             }
-            
         }
 
-        private onLogInError = (): void=>
+        private onLogInError = (): void =>
         {
             this.loginFailed = true;
             this.password = "";
