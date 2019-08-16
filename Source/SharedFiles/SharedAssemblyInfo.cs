b373b7cb4a4e053b0db9d3154afd9860efb56164
parkkeer <eero.parkkonen@cgi.com>
2019-08-16 11:54:44 +0300

    Version to 0.28.0

--- Source/SharedFiles/SharedAssemblyInfo.cs
+++ Source/SharedFiles/SharedAssemblyInfo.cs
@@ -2,6 +2,6 @@
 
 [assembly: AssemblyProduct("Organisaatiorekisteri")]
 
-[assembly: AssemblyVersion("0.27.0.0")]
-[assembly: AssemblyFileVersion("0.27.0.0")]
-[assembly: AssemblyInformationalVersion("0.27.0")]
+[assembly: AssemblyVersion("0.28.0.0")]
+[assembly: AssemblyFileVersion("0.28.0.0")]
+[assembly: AssemblyInformationalVersion("0.28.0")]
